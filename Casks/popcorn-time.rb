2def41d95c61211673637de390104006b845961e
Sepehr Lajevardi <lajevardi@gmail.com>
2016-12-19 17:39:35 +0330

    Cask style fix.

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -2,7 +2,6 @@ cask 'popcorn-time' do
   version '0.3.10'
   sha256 '62002814ddac7587e447d0f8e7db5d7359ccca77893b79223f7b1f5c7d8992bf'
 
-  # get.popcorntime.sh was verified as official when first introduced to the cask
   url "https://get.popcorntime.sh/repo/build/Popcorn-Time-#{version}-Mac.zip"
   name 'Popcorn Time'
   homepage 'https://popcorntime.sh/'
@@ -10,9 +9,9 @@ cask 'popcorn-time' do
   app 'Popcorn-Time.app'
 
   zap delete: [
-    '~/Library/Caches/Popcorn-Time',
-    '~/Library/Application Support/Popcorn-Time',
-    '~/Library/Preferences/com.nw-builder.popcorn-time.plist',
-    '~/Library/Saved Application State/com.nw-builder.popcorn-time.savedState',
-  ]
+                '~/Library/Caches/Popcorn-Time',
+                '~/Library/Application Support/Popcorn-Time',
+                '~/Library/Preferences/com.nw-builder.popcorn-time.plist',
+                '~/Library/Saved Application State/com.nw-builder.popcorn-time.savedState',
+              ]
 end
