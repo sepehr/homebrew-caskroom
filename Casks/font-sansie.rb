b5bf9dc6bf7d6dda4025a2ed75660c86a9433a24
Sepehr Lajevardi <lajevardi@gmail.com>
2016-12-19 17:45:41 +0330

    Styled and audited.

--- Casks/font-sansie.rb
+++ Casks/font-sansie.rb
@@ -1,9 +1,10 @@
 cask 'font-sansie' do
   version :latest
-  sha256 '599b8e5780103b66199120d7b809f09f61cf06ff781d97347ec809f29c1cfd06'
+  sha256 :no_check
 
   url 'http://srv70.putdrive.com/putstorage/DownloadFileHash/5A747D843A5A4A5QQWE2308071EWQS/Sansie.zip'
   name 'Sansie'
+  homepage 'https://putdrive.com/'
 
   font 'Sansie/Sansie.ttf'
   font 'Sansie/Sansie-Bold.ttf'
