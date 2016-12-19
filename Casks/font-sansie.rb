cc094861d16500cafd4833260b47d991e6dbd454
Sepehr Lajevardi <lajevardi@gmail.com>
2016-12-19 15:16:34 +0330

    Add sansie font.

---
+++ Casks/font-sansie.rb
@@ -0,0 +1,13 @@
+cask 'font-sansie' do
+  version :latest
+  sha256 '599b8e5780103b66199120d7b809f09f61cf06ff781d97347ec809f29c1cfd06'
+
+  url 'http://srv70.putdrive.com/putstorage/DownloadFileHash/5A747D843A5A4A5QQWE2308071EWQS/Sansie.zip'
+  name 'Sansie'
+
+  font 'Sansie/Sansie.ttf'
+  font 'Sansie/Sansie-Bold.ttf'
+  font 'Sansie/Sansie-Light.ttf'
+  font 'Sansie/Sansie-Medium.ttf'
+  font 'Sansie/Sansie-UltraLight.ttf'
+end
