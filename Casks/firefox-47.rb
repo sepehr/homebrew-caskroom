9099d44b3891a1dfc9d81f2689836136669f26e7
Sepehr Lajevardi <lajevardi@gmail.com>
2016-12-26 15:35:14 +0330

    Add firefox-47 cask.

---
+++ Casks/firefox-47.rb
@@ -0,0 +1,17 @@
+cask 'firefox-47' do
+  version :latest
+  sha256 :no_check
+
+  url "https://ftp.mozilla.org/pub/firefox/releases/47.0.1/mac/en-US/Firefox%2047.0.1.dmg"
+  name 'Mozilla Firefox'
+  homepage 'https://www.mozilla.org/firefox/'
+
+  auto_updates false
+
+  app 'Firefox.app'
+
+  zap delete: [
+                '~/Library/Application Support/Firefox',
+                '~/Library/Caches/Firefox',
+              ]
+end
