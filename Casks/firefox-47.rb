0fa89f489708423021643493fdaa46d5ca29b801
Sepehr Lajevardi <lajevardi@gmail.com>
2016-12-26 15:49:04 +0330

    Improve firefox-47 cask.

--- Casks/firefox-47.rb
+++ Casks/firefox-47.rb
@@ -2,11 +2,12 @@ cask 'firefox-47' do
   version :latest
   sha256 :no_check
 
-  url "https://ftp.mozilla.org/pub/firefox/releases/47.0.1/mac/en-US/Firefox%2047.0.1.dmg"
+  url 'https://ftp.mozilla.org/pub/firefox/releases/47.0.1/mac/en-US/Firefox%2047.0.1.dmg'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/firefox/'
 
   auto_updates false
+  conflicts_with cask: 'firefox'
 
   app 'Firefox.app'
 
